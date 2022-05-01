cf1d9d8f73572791a4136a703ffb844ded35bc67
Gabriel Coronado Pacheco <gabrielcp14@outlook.com>
2022-05-01 14:25:11 -0300

    ok

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,37 @@
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './components/Home';
+import NewProject from './components/NewProject';
+import Company from './components/Company';
+import Contact from './components/Contact';
+
+import Container from './components/layout/Container';
+
 function App() {
     return(
-      <div className="App">
-        <p>Costs</p>
-      </div>
+     <Router>
+       <div>
+         <Link to="/">Home</Link>
+         <Link to="/contact">Contato</Link>
+         <Link to="/company">Empresa</Link>
+         <Link to="/newproject">Novo Projeto</Link>
+       </div>
+       <Switch>
+          <Container>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route exact path="/company">
+              <Company/>
+            </Route>
+            <Route exact path="/contact">
+              <Contact/>
+            </Route>
+            <Route exact path="/newproject">
+              <NewProject/>
+            </Route>
+          </Container>
+       </Switch>
+     </Router>
     ) 
 }
 
---
+++ costs/src/components/layout/Container.js
@@ -0,0 +1,8 @@
+import styles from './Container.module.css'
+
+function Container(props){
+    return <div>{props.children}</div>
+
+}
+
+export default Container
