351c8a948a06610ced8064daaa99f444d7849804
Gabriel Coronado Pacheco <gabrielcp14@outlook.com>
2022-05-06 17:39:17 -0300

    ok

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,8 +1,25 @@
+import {useState} from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 function ProjectForm({btnText}){
+
+    const [categories,setCategories] = UseState([])
+
+    fetch("http://localhost:5000/categories",{
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json',
+        },
+    })
+     .then((resp) => resp.json())
+     .then((data) => {
+         setCategories(data)
+     })
+     .catch((err) => console.log(err))
+
     return (
         <form className={styles.form}>
            <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto...'  />
